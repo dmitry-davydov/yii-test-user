f331f6dc330a1df322c287f4ab9004e874affbac
Dmitry <Dmitry@laptop.local>
2015-07-24 16:10:30 +0300

    ~

--- Module.php
+++ Module.php
@@ -5,8 +5,6 @@ namespace ddavydov;
 
 class Module extends \yii\base\Module
 {
-  public $controllerNamespace = 'admin\modules\users\controllers';
-
   public function init()
   {
     parent::init();
