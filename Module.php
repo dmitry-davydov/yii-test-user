0cf0917fd9ed13c864de038343dbaf2cffd31734
Dmitry <Dmitry@laptop.local>
2015-07-24 13:39:54 +0300

    initial

---
+++ Module.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace ddavydov\yiitestuser;
+
+
+class Module extends \yii\base\Module
+{
+  public $controllerNamespace = 'admin\modules\users\controllers';
+
+  public function init()
+  {
+    parent::init();
+  }
+}
---
+++ controllers/TestController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace ddavydov\yiitestuser\controllers;
+
+use yii\helpers\VarDumper;
+use yii\web\Controller;
+
+class TestController extends Controller
+{
+  /**
+   * @inheritDoc
+   */
+  public function __construct($id, $module, $config = [])
+  {
+    parent::__construct($id, $module, $config);
+  }
+
+
+  public function actionTest()
+  {
+    VarDumper::dump($this, 10, true);
+  }
+}
